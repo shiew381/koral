cbaedab8795a5fd05939b683679ae678f5592843
shiew381 <shiew381@gmail.com>
2022-01-23 08:55:54 -0800

    cleanup default create-react-app files and create app folder

---
+++ src/app/App.jsx
@@ -0,0 +1,3 @@
+export default function App() {
+  return "initializing app and remote repository";
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app/App.jsx";
+// import "katex/dist/katex.min.css";
+// import "./css/suneditor.css";
+// import "./css/styles.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// if (process.env.REACT_APP_PRODUCT === "koi") {
+//   require("./css/styles-koi.css");
+// } else {
+//   require("./css/styles-koral.css");
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(<App />, rootElement);
